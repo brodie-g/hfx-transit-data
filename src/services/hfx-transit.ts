42b58b0198003186eb0637eabc35cca2fe25c2e0
bgallinger <brodie.gallinger@nielsen.com>
2019-02-06 09:27:26 -0400

    - use open data APIs as data sources

--- src/services/hfx-transit.ts
+++ src/services/hfx-transit.ts
@@ -1,19 +1,28 @@
 import http from 'axios';
 import {TransitSystem} from '../models/TransitSystem';
 
+const dataEndpoints = {
+    ROUTES: 'https://opendata.arcgis.com/datasets/e3b2bfdd61154176822c00602504c950_0.geojson',
+    PASSENGERS: 'https://opendata.arcgis.com/datasets/913b0ec9a2994df7ac6ae58357ce8117_0.geojson',
+};
+
 export async function getTransitRoutes() {
-    const geojsonRes = await http.get('/data/halifax-transit-routes.geojson');
+    // get route data from the API
+    const geojsonRes = await http.get(dataEndpoints.ROUTES);
+
     return geojsonRes.data;
 }
 
 export async function getTransitRoutePassengers() {
-    const passengersRes = await http.get('/data/halifax-transit-route-passengers.geojson');
+    // get the passenger data from the API
+    const passengersRes = await http.get(dataEndpoints.PASSENGERS);
+
     return passengersRes.data;
 }
 
 export async function getTransitSystem() {
-    const geoJSON = await getTransitRoutes();
-    const passengerData = await getTransitRoutePassengers();
+    // run API calls in parallel. TODO: error handling
+    const [geoJSON, passengerData] = await Promise.all([getTransitRoutes(), getTransitRoutePassengers()]);
 
     const system = new TransitSystem(geoJSON);
     system.addPassengerData(passengerData);
