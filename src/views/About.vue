43a013f6ced7b30dbe13dec7df64869a4fd223de
brodie <brodie.gallinger@gmail.com>
2019-02-28 22:07:40 -0400

    - update about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h2>This is an about page</h2>
+    <h4>This is an application to display Halifax Transit Open Data. I could not find another place to easily and interactively see all mapped bus routes. So here it is.</h4>
+    <h4>All data is fetched from <a target="_blank" href="http://catalogue-hrm.opendata.arcgis.com/datasets/bus-routes">Halifax Open Data</a></h4>
   </div>
 </template>
